db27ac65e397f6da0b4486ca087b747142c0642e
jiratsun <57980067+jiratsun@users.noreply.github.com>
2019-12-07 16:49:29 +0700

    firstCommit

--- MarsApp/src/app/home/home.page.ts
+++ MarsApp/src/app/home/home.page.ts
@@ -3,18 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  splash = true;
+  tabBarElement: any;
 
-  hidevalue = false;
-
-  ngOnInit() {
-    setTimeout(x => {
-      this.hidevalue = true;
-    }, 3000);
+  constructor() {
+    this.tabBarElement = document.querySelector('ion-tab-bar');
   }
 
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 2500);
+  }
+  ngOnInit(){
+    this.ionViewDidLoad()
+  }
 }
